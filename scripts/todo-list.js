9cfa4316e7f37d77e8232deec56f49d76afb93e6
CourtSC <scott.c.court@gmail.com>
2023-11-16 16:37:11 -0500

    Added listener to inject html for todo list button on renderPlayerList event.

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -4,11 +4,11 @@
  * @property {string} id - A unique ID to identify this todo.
  * @property {string} label - the text of the todo.
  * @property {boolean} isDone - Marks whether the todo is done.
- * @property {string} userId - The user who owns this todo.
+ * @property {string} userID - The user who owns this todo.
  */
 
 class toDoList {
-	static ID = 'todo-list';
+	static ID = 'FoundryVTT-ToDo-List';
 
 	static FLAGS = {
 		TODOS: 'todos',
@@ -18,3 +18,86 @@ class toDoList {
 		TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`,
 	};
 }
+
+class toDoListData {
+	static getToDosForUser(userID) {
+		return game.users.get(userID)?.getFlag(toDoList.ID, toDoList.FLAGS.TODOS);
+	}
+
+	static createToDo(userID, toDoData) {
+		//generate a random ID for this todo and populate the userID
+		const newToDo = {
+			isDone: false,
+			...toDoData,
+			id: foundry.utils.randomID(16),
+			userID,
+		};
+
+		// construct the update to insert the new ToDo
+		const newToDos = {
+			[newToDo.id]: newToDo,
+		};
+
+		//update the database with the new ToDos
+		return game.users
+			.get(userID)
+			?.setFlag(toDoList.ID, toDoList.FLAGS.TODOS, newToDos);
+	}
+
+	static get allToDos() {
+		const allToDos = game.users.reduce((accumulator, user) => {
+			const userTodos = this.getToDosForUser(user.id);
+
+			return {
+				...accumulator,
+				...userTodos,
+			};
+		}, {});
+
+		return allToDos;
+	}
+
+	static updateToDo(toDoID, updateData) {
+		const relevantToDo = this.allToDos[toDoID];
+
+		//construct the update to send
+		const update = {
+			[toDoID]: updateData,
+		};
+
+		// update the database with the updated ToDo list
+		return game.users
+			.get(relevantToDo.userID)
+			?.setFlag(toDoList.ID, toDoList.FLAGS.TODOS, update);
+	}
+
+	static deleteToDo(toDoID) {
+		const relevantToDo = this.allToDos[toDoID];
+
+		// Foundry specific syntax required to delete a key from a persisted object in the database
+		const keyDeletion = {
+			[`-=${toDoID}`]: null,
+		};
+
+		//update the database with the updated ToDo list
+		return game.users
+			.get(relevantToDo.userID)
+			?.setFlag(toDoList.ID, toDoList.FLAGS.TODOS, keyDeletion);
+	}
+
+	static updateUserToDos(userID, updateData) {
+		return game.users
+			.get(userID)
+			?.setFlag(toDoList.ID, toDoList.FLAGS.TODOS, updateData);
+	}
+}
+
+Hooks.on('renderPlayerList', (playerList, html) => {
+	// find the element which has our logged in user's id
+	const loggedInUserListItem = html.find(`[data-user-id="${game.userId}"]`);
+
+	// insert a button at the end of this element
+	loggedInUserListItem.append(
+		"<button type='button' class='todo-list-icon-button'><i class='fas fa-tasks'></i></button>"
+	);
+});
