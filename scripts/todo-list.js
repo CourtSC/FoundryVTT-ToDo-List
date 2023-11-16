9acb82ab9ee73c86621e64bcea522d20636d261a
CourtSC <scott.c.court@gmail.com>
2023-11-16 15:15:46 -0500

    Definied toDoList class.

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,1 +1,20 @@
-console.log('to-do list | Hello World!');
+/**
+ * A single ToDo in our list of Todos.
+ * @typedef {Object} ToDo
+ * @property {string} id - A unique ID to identify this todo.
+ * @property {string} label - the text of the todo.
+ * @property {boolean} isDone - Marks whether the todo is done.
+ * @property {string} userId - The user who owns this todo.
+ */
+
+class toDoList {
+	static ID = 'todo-list';
+
+	static FLAGS = {
+		TODOS: 'todos',
+	};
+
+	static TEMPLATES = {
+		TODOLIST: `modules/${this.ID}/templates/todo-list.hbs`,
+	};
+}
